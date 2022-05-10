406298e8e41b7247cb68cb0f4831cb998ea81dd3
ye0215 <ji_jiayan@rivotek.com>
2022-05-10 20:29:37 +0800

    list

---
+++ test/0510/list.c
@@ -0,0 +1,84 @@
+ÐÏà¡??                >  ?	                               ?ÿÿ        ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ?ÿÿ   ?ÿÿ               	   
+               ?ÿÿ   ?ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿR o o t   E n t r y                                               ÿÿÿÿÿÿÿÿ                                gcZd?   @      D a t a                                                         
+ ÿÿÿÿÿÿÿÿÿÿÿÿ                                    ?ÿÿ        0 T a b l e                                                            ÿÿÿÿ                                        .      W o r d D o c u m e n t                                           ÿÿÿÿÿÿÿÿÿÿÿÿ                                       3                              	   
+                                                         ?ÿÿ          !   "   #   $   %   &   '   (   )   *   +   ,   -   .   /   0   1   2   3   4   5   6   7   8   9   :   ;   <   =   >   ?   @   A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   ?ÿÿW   X   Y   Z   [   d   ]   ^   _   `   a   b   c   ?ÿÿ?ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ    ?          F  `? F            ck‡e     a$$1$   CJ PJ KH mH	sH	nHtH                  $ A`?? $            Øž¤‹µk=„W[SO            8  ` ? 8            u??    a$ $ G$ ? 9r  CJ ? ` ?            u?w  c  d?  a$$G$ @&	$d   N?   ÿ   %d   O?   ÿ   &d   P?   ÿ   'd   Q?   ÿ   ? 9r   CJ OJ  QJ                                   	   G?  ‡z     €       ÿ      T i m e s   N e w   R o m a n   -??       ?             ‹[SO  1?? ? €R<(             ®_oÅ–Ñ?  5?                    €    S y m b o l   3$? ÿ* àCx ?       ÿ @  ÿÿA r i a l   -?? 	? €ú|?             ÑžSO  ?4? 	ÿ* àCx ?       ÿ @  ÿÿC o u r i e r   N e w   ;?              #include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+    int value;
+    struct Node *next;
+};
+
+int get_lenth_of_list( struct Node *head )
+{
+struct Node  *temp = &top;
+Int i;
+while(top->next != NULL)
+{
+i++;
+}
+i++;
+}
+
+struct Node *build_list( int value )
+{
+    struct Node *top = (struct Node *)malloc( sizeof( struct Node ) );
+    top->value = value;
+    
+    if(top != NULL)
+        return top;
+    else
+        return NULL;
+}
+
+int destroy_list(struct Node *top)
+{
+list *freeNode;
+while (NULL != top){
+freeNode = top;
+top =top-> next;
+free(freeNode);
+Printf(¡°ÊÍ·Å%d½áµã¡±£¬freeNode);
+}
+
+int insert_node(struct Node **top, int new_value)
+{
+    if (NULL == h)
+    {
+        return FALSE;
+    }
+    Node* node = (Node*) malloc(sizeof(Node) / sizeof(char));
+    if (NULL == node)
+    {
+        return FALSE;
+    }
+    node->data = data;
+    node->next = h->next;   
+    h->next = node;
+    return TRUE;
+
+}
+
+void print_list(struct Node *top)
+{
+   
+}
+
+
+int main(void)
+{
+    struct Node *top = build_list(2);
+
+    insert_node(&top, 4);
+    insert_node(&top, 1);
+    insert_node(&top, 7);
+    insert_node(&top, 2);
+
+    print_list(top);
+    printf("%d\n", get_lenth_of_list(top));  
+    destroy_list(top);
+
+    return 0;
+}
+
+                                                                                                                                                                                      
